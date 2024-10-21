c95a8afe4be6122c3500885689cbd0fe48efc2e4
RajithaKWG <rkrishan894@gmail.com>
2024-10-21 17:25:44 +0530

    setup main.jsx

--- first-redux-app/src/main.jsx
+++ first-redux-app/src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {} from 'r'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-  
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
